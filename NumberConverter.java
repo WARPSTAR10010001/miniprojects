e7fa5636a7d5b5bd4a8c4f4d6fd7026c05b748b1
Warpstar10010001 <alpay.gueruez@hsrw.org>
2024-12-19 12:28:44 +0100

    Create NumberConverter.java

---
+++ NumberConverter.java
@@ -0,0 +1,11 @@
+//Boo!!
+
+import java.util.Scanner;
+
+public class NumberConverter {
+
+    static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        //code...
+    }
+}
