aa91c7497f06c266973b9950790299f6f3d65ea1
Warpstar10010001 <alpay.gueruez@hsrw.org>
2024-12-28 03:32:32 +0100

    Create Main.java

---
+++ FitnessCalculatorPlus/Main.java
@@ -0,0 +1,12 @@
+//Boo!!
+
+package FitnessCalculatorPlus;
+
+public class Main {
+
+    static UserInterface UI = new UserInterface();
+    static DataCalculator Calculator = new DataCalculator();
+    public static void main(String[] args) {
+        UI.start();
+    }
+}
