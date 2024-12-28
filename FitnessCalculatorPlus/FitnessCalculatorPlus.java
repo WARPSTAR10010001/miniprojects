8c99bedb7c2d544da88e8f8cd409970828396721
Warpstar10010001 <alpay.gueruez@hsrw.org>
2024-12-28 03:01:48 +0100

    Create FitnessCalculatorPlus.java

---
+++ FitnessCalculatorPlus/FitnessCalculatorPlus.java
@@ -0,0 +1,11 @@
+//Boo!!
+
+package FitnessCalculatorPlus;
+
+public class FitnessCalculatorPlus {
+    public static void main(String[] args) {
+        FitnessCalculator BMFitnessCalculator = new FitnessCalculator();
+
+        FitnessCalculator.bmi();
+    }
+}
