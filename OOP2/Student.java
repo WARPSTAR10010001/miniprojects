08eee9c8067361489cebc85fc98cadfff90b4264
WARPSTAR <alpay.gueruez@hsrw.org>
2024-12-29 06:19:55 +0100

    Update Student.java

--- OOP2/Student.java
+++ OOP2/Student.java
@@ -1,6 +1,23 @@
 package OOP2;
 
 class Student extends Person {
+    private int studentID;
 
-    int studentID;
+    public Student(int age, String name, boolean isMale, int studentID){
+        super(age, name, isMale);
+        this.studentID = studentID;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    @Override
+    public String toString(){
+        return "Hi, I'm " + getName() + ", I'm " + getAge() + " years old and my student ID is " + this.studentID;
+    }
 }
