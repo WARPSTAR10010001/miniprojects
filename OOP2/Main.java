98c97b1515a4e474a3e973a99b09a544cf47a11d
Warpstar10010001 <alpay.gueruez@hsrw.org>
2024-12-29 17:38:43 +0100

    Update Main.java

--- OOP2/Main.java
+++ OOP2/Main.java
@@ -7,17 +7,17 @@ package OOP2;
 public class Main {
 
     public static void main(String[] args){
-        Student WARPSTAR = new Student(18, "WARPSTAR", true, 35191);
-        Student Steven = new Student(69, "Steve", true, 69420);
+        Student WARPSTAR = new Student(18, "WARPSTAR", true);
+        Student Steven = new Student(69, "Steve", true);
         Person UdoLindenberg = new Person(99, "Udo Lindenberg", true);
-        Student Gurkenfresser = new Student(12, "Gurkenfresser", true, 10101);
+        Student Gurkenfresser = new Student(12, "Gurkenfresser", true);
         Person SaddamHussein = new Person(128, "Saddam Hussein", true);
         FirstSemesterStudent FortniteGamer233 = new FirstSemesterStudent(15, "Johannes von Brandenburg (der Idiot)", true, 55555, 1);
 
         Person [] personArray = {WARPSTAR, UdoLindenberg, Steven, Gurkenfresser, SaddamHussein, FortniteGamer233};
 
-        for (Person personArray1 : personArray) {
-            System.out.println(personArray1);
+        for(int i = 0; i < personArray.length; i++){
+            System.out.println(personArray[i]);
         }
     }
 }
