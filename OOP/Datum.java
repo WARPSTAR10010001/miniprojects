48ffac167adeb20f3d6857d68e90851c762f12da
WARPSTAR <alpay.gueruez@hsrw.org>
2024-12-29 05:40:14 +0100

    Update Datum.java

--- OOP/Datum.java
+++ OOP/Datum.java
@@ -17,7 +17,40 @@ public class Datum {
         this.jahr = jahr;
     }
 
+    @Override
     public String toString(){
         return name + ", den " + tag + "." + monat + "." + jahr;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getTag() {
+        return tag;
+    }
+
+    public void setTag(int tag) {
+        this.tag = tag;
+    }
+
+    public int getMonat() {
+        return monat;
+    }
+
+    public void setMonat(int monat) {
+        this.monat = monat;
+    }
+
+    public int getJahr() {
+        return jahr;
+    }
+
+    public void setJahr(int jahr) {
+        this.jahr = jahr;
     }
 }
