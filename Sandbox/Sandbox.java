a77144aead89eb68c2a973b3bda81967b6573bbb
WARPSTAR <alpay.gueruez@hsrw.org>
2024-12-29 05:40:27 +0100

    Create Sandbox.java

---
+++ Sandbox/Sandbox.java
@@ -0,0 +1,16 @@
+//Boo!!
+
+public class Sandbox {
+
+    public static void main(String[] args){
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int[] intArray = {a, b, c};
+
+        for(int i = 0; i < intArray.length; i++){
+            System.out.println(intArray[i]);
+        }
+    }
+}
