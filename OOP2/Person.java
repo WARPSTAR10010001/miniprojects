0b6ad06f53a680d8e136d2d174616920263045e4
WARPSTAR <alpay.gueruez@hsrw.org>
2024-12-29 05:40:20 +0100

    Create Person.java

---
+++ OOP2/Person.java
@@ -0,0 +1,13 @@
+package OOP2;
+
+public class Person {
+    int age;
+    String name;
+    boolean isMale;
+
+    public Person(int age, String name, boolean isMale){
+        this.age = age;
+        this.name = name;
+        this.isMale = isMale;
+    }
+}
