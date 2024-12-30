c45377e65e896d4a2a91b48c4f07277f5a9e5051
Warpstar10010001 <alpay.gueruez@hsrw.org>
2024-12-30 13:25:02 +0100

    Update Menu.java

--- Miniprojekt4/Menu.java
+++ Miniprojekt4/Menu.java
@@ -1,2 +1,10 @@
+//Boo!!
+
+package Miniprojekt4;
+
 public class Menu {
+    public static void main(String[] args) {
+        Artikel Monster = new Artikel("Monster Energy (Femboy Edition)", 1.20, 19);
+        System.out.println(Monster);
+    }
 }
