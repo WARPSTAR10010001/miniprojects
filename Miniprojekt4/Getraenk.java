8b31c6870b77242858d1086ea785508dc849e9b1
Warpstar10010001 <alpay.gueruez@hsrw.org>
2024-12-30 13:24:59 +0100

    Update Getraenk.java

--- Miniprojekt4/Getraenk.java
+++ Miniprojekt4/Getraenk.java
@@ -1,2 +1,28 @@
-public class Getraenk {
+//Boo!!
+
+package Miniprojekt4;
+
+public class Getraenk extends Artikel{
+    private double pfand;
+
+    public Getraenk(){
+        super();
+        this.pfand = 0;
+    }
+
+    public Getraenk(String name, double preis, double mwstsatz, double pfand){
+        super(name, preis, mwstsatz);
+        this.pfand = pfand;
+    }
+
+    public double getPfand(){
+        return this.pfand;
+    }
+
+    private void setPfand(double pfand){
+        this.pfand = pfand;
+    }
+
+    @Override
+    
 }
