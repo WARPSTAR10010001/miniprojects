c2d99b04ea4d8d069ce57d42c35eba8023197a42
Warpstar10010001 <alpay.gueruez@hsrw.org>
2024-12-30 13:24:56 +0100

    Update Artikel.java

--- Miniprojekt4/Artikel.java
+++ Miniprojekt4/Artikel.java
@@ -1,3 +1,55 @@
-public class Artikel {
+//Boo!!
 
+package Miniprojekt4;
+
+public class Artikel {
+    private String name;
+    private double preis;
+    private double mwstsatz;
+
+    public Artikel(){
+        this.name = null;
+        this.preis = 0;
+        this.mwstsatz = 0;
+    }
+
+    public Artikel(String name, double preis, double mwstsatz){
+        this.name = name;
+        this.preis = preis;
+        this.mwstsatz = mwstsatz;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPreis(){
+        return this.preis;
+    }
+
+    public void setPreis(double preis){
+        this.preis = preis;
+    }
+
+    public double getMwstsatz(){
+        return this.mwstsatz;
+    }
+
+    public void setMwstsatz(double mwstsatz){
+        this.mwstsatz = mwstsatz;
+    }
+
+    public double berechneMwst() {
+        double mwst = this.preis * (this.mwstsatz / 100);
+        return Math.round(mwst * 100.0) / 100.0;
+    }
+
+    @Override
+    public String toString(){
+        return this.name + " - " + this.preis + " Euro (inkl. " + berechneMwst() + " Euro Mwst.)";
+    }
 }
