88beac9b667ebc126ed606c21c6cb68d673625ca
WARPSTAR <alpay.gueruez@hsrw.org>
2025-01-02 00:03:22 +0100

    Update Main.java

--- FitnessCalculatorPlus/Main.java
+++ FitnessCalculatorPlus/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
     static UserInterface UI = new UserInterface();
     static DataCalculator Calculator = new DataCalculator();
+    static User User = new User();
     public static void main(String[] args) {
         UI.start();
     }
