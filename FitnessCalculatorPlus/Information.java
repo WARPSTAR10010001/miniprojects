481bbcbd406b71878e3ac0c2110556bd26dc756e
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-10 02:27:25 +0100

    Create Information.java

---
+++ FitnessCalculatorPlus/Information.java
@@ -0,0 +1,16 @@
+//Boo!!
+
+package FitnessCalculatorPlus;
+
+public class Information {
+    private final String version = "2.0";
+	private final String lastUpdate = "10.01.24";
+
+    public String getVersion(){
+        return this.version;
+    }
+
+    public String getLastUpdate(){
+        return this.lastUpdate;
+    }
+}
