af06b3e5f805f53338182f247dc3d0a4215be640
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-14 19:35:54 +0100

    Create ArrayDelete.java

---
+++ Probeklausur/ArrayDelete.java
@@ -0,0 +1,59 @@
+//Boo!!
+
+package Probeklausur;
+
+import java.util.Arrays;
+
+public class ArrayDelete {
+	/**
+	 * Die Methode nimmt ein Array und einen Wert entgegen und soll eine Kopie des
+	 * übergebenen Arrays zurückgeben, in dem alle Vorkommen des Wertes nicht mehr
+	 * enthalten sind. Die Methode soll das Originalarray unverändert lassen.
+	 *
+	 * Beispiel: Eingabe = {4, -3, 17, 6, 0, -3, 4, 9}, -3 Rückgabe = {4, 17, 6, 0,
+	 * 4, 9}
+	 *
+	 * @param a    Das Array, das kopiert werden soll
+	 * @param wert Wert, der nicht in der Kopie enthalten sein soll
+	 * @return Das kopierte Array mit den verbliebenen Elementen
+	 */
+
+	public static int[] deleteElements(int[] a, int wert) {
+		int[] temp = new int [a.length];
+		Arrays.fill(temp, Integer.MIN_VALUE);
+
+		for(int i = 0; i < a.length; i++){
+			if(a[i] != wert){
+				temp[i] = a[i];
+			}
+		}
+
+		int count = 0;
+
+		for(int i = 0; i < temp.length; i++){
+			if(temp[i] != Integer.MIN_VALUE){
+				count++;
+			}
+		}
+
+		int[] result = new int [count];
+
+		int init = 0;
+		for(int i = 0; i < temp.length; i++){
+			if(temp[i] != Integer.MIN_VALUE){
+				result[init] = temp[i];
+				init++;
+			}
+		}
+
+		return result;
+	}
+
+	// Verwenden Sie die Methode main für Ihre Tests, fügen Sie ggf.
+	// weitere Tests hinzu
+	public static void main(String[] args) {
+		int[] a = { 4, -3, 17, 6, 0, -3, 4, 9 };
+		// Sollte [4, 17, 6, 0, 4, 9] ausgeben:
+		System.out.println(Arrays.toString(ArrayDelete.deleteElements(a, -3)));
+	}
+}
