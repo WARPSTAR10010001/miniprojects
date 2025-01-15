cc9bac25ea87486b87d37398b8ac8461ce79ac97
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-15 16:09:33 +0100

    Create Guthabenkonto.java

---
+++ SOOP/Probeklausur/Guthabenkonto.java
@@ -0,0 +1,14 @@
+//Boo!!
+
+package Probeklausur;
+
+// Lösung in dieser Datei
+public class Guthabenkonto extends Konto {
+    public Guthabenkonto(){
+        super();
+    }
+
+    public Guthabenkonto(double guthaben){
+        super(guthaben, 0);
+    }
+}
