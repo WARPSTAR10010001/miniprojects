84ca29ff122504d55dad50c10fe56d5c112dabdf
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-16 00:01:31 +0100

    Update Artikel.java

--- SOOP/Miniprojekt4/Artikel.java
+++ SOOP/Miniprojekt4/Artikel.java
@@ -1,6 +1,6 @@
 //Boo!!
 
-package Miniprojekt4;
+//package Miniprojekt4;
 
 public class Artikel {
     private String name;
@@ -9,7 +9,7 @@ public class Artikel {
 
     public Artikel(){
         this.name = "N/A";
-        this.preis = -1;
+        this.preis = 0;
         this.mwstsatz = 0;
     }
 
@@ -19,37 +19,37 @@ public class Artikel {
         this.mwstsatz = mwstsatz;
     }
 
-    public String getName(){
-        return this.name;
+    public String getName() {
+        return name;
     }
 
-    public void setName(String name){
+    public void setName(String name) {
         this.name = name;
     }
 
-    public double getPreis(){
-        return this.preis;
+    public double getPreis() {
+        return preis;
     }
 
-    public void setPreis(double preis){
+    public void setPreis(double preis) {
         this.preis = preis;
     }
 
-    public double getMwstsatz(){
-        return this.mwstsatz;
+    public double getMwstsatz() {
+        return mwstsatz;
     }
 
-    public void setMwstsatz(double mwstsatz){
+    public void setMwstsatz(double mwstsatz) {
         this.mwstsatz = mwstsatz;
     }
 
     public double berechneMwst() {
-        double mwst = this.preis * (this.mwstsatz / 100);
+        double mwst = this.preis * this.mwstsatz;
         return Math.round(mwst * 100.0) / 100.0;
     }
 
     @Override
     public String toString(){
-        return this.name + " - " + this.preis + " Euro (inkl. " + berechneMwst() + " Euro Mwst.)";
+        return String.format("%s - %.2f Euro (inkl. %.2f Euro Mwst.)", this.name, this.preis, berechneMwst());
     }
 }
