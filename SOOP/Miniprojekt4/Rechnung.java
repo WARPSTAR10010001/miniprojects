2bc0568e4ed17e6821fec78a510ae937d72658f5
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-16 00:01:38 +0100

    Update Rechnung.java

--- SOOP/Miniprojekt4/Rechnung.java
+++ SOOP/Miniprojekt4/Rechnung.java
@@ -1,30 +1,44 @@
 //Boo!!
 
-package Miniprojekt4;
+//package Miniprojekt4;
+
+import java.util.Arrays;
 
 public class Rechnung {
     private Artikel[] artikel;
 
     public Rechnung(){
-        this.artikel = new Artikel[Integer.MAX_VALUE];
+        this.artikel = new Artikel[100];
+        Artikel zero = new Artikel();
+        Arrays.fill(artikel, zero);
+
+        
     }
 
-    public Artikel[] getArtikel(){
-        return this.artikel;
+    public Artikel[] getArtikel() {
+        return artikel;
     }
 
-    public void setArtikel(Artikel[] artikel){
+    public void setArtikel(Artikel[] artikel) {
         this.artikel = artikel;
     }
 
     public void neuerArtikel(Artikel artikel){
-        
+        int index = 0;
+        while(!(this.artikel[index].getName().equals("N/A"))){
+            index++;
+        }
+
+        this.artikel[index ] = artikel;
     }
 
     public double getBetrag(){
         double betrag = 0;
-        for(int i = 0; i < artikel.length; i++){
-            betrag += artikel[i].getPreis();
+
+        for(int i = 0; i < this.artikel.length; i++){
+            if(!(this.artikel[i].getName().equals("N/A"))){
+                betrag += this.artikel[i].getPreis();
+            }
         }
 
         return betrag;
