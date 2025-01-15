dd29f27753e31dea460cbb9e94edecb7c87177d9
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-15 16:17:36 +0100

    Create Information.java

---
+++ #FitnessCalculatorPlus/Information.java
@@ -0,0 +1,24 @@
+//Boo!!
+
+package #FitnessCalculatorPlus;
+
+import java.util.Scanner;
+
+public class Information {
+    private final String version = "2.0";
+	private final String lastUpdate = "11.01.24";
+    private final String divider = "----------------------------------------";
+    public Scanner scanner = new Scanner(System.in);
+
+    public String getVersion(){
+        return this.version;
+    }
+
+    public String getLastUpdate(){
+        return this.lastUpdate;
+    }
+
+    public String getDivider(){
+        return this.divider;
+    }
+}
