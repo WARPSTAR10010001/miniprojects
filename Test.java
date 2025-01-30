91a9f207dce325c9440371f0b86f5a1fe1d9eafc
Warpstar10010001 <alpay.gueruez@hsrw.org>
2025-01-30 03:26:56 +0100

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,38 +1,71 @@
 //Boo!!
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Test {
     public static void main(String[] args) {
-        Mensch p = new Mensch("Philip Amthor", "Blau", 32);
-        Mensch t = new Mensch("Olaf Scholz", "Rot", 66);
+        ArrayList<Mensch> gruppe = new ArrayList<>();
+        Prof richter = new Prof("T. Richter", "männlich", 44);
+        Prof greveler = new Prof("U. Greveler", "männlich", 52);
+        Prof zimmer = new Prof("F. Zimmer", "männlich", 55);
+        
+        Student warpstar = new Student("WARPSTAR", 18, "weiberlich");
+        Student herbert = new Student("Herbert", 19, "männlich");
+        Student bomboclaat = new Student("Bomboclaat", 21, "männlich");
+        
+        Mitarbeiter lennartz = new Mitarbeiter("F. Lennartz", 31, "männlich");
 
-        Mensch[] gruppeArray = new Mensch[2];
-
-        gruppeArray[0] = p;
-        gruppeArray[1] = t;
-
-        for(int i = 0; i < gruppeArray.length; i++){
-            System.out.println(gruppeArray[i].toString());
-        }
-
-        System.out.println("\n----------------------\n");
-
-        ArrayList<Mensch> gruppe = new ArrayList<>(Arrays.asList(gruppeArray));
-
-        gruppe.add(new Prof("Thomas Richter", "Hellblau", 44, 1213));
+        gruppe.add(richter);
+        gruppe.add(greveler);
+        gruppe.add(zimmer);
+        gruppe.add(warpstar);
+        gruppe.add(herbert);
+        gruppe.add(bomboclaat);
+        gruppe.add(lennartz);
 
         for(int i = 0; i < gruppe.size(); i++){
-            System.out.println(gruppe.get(i));
+            System.out.println(gruppe.get(i).toString());
+        }
+
+        System.out.println("\n------------------------------\n");
+
+        ArrayList<Prof> proflist = new ArrayList<>();
+        ArrayList<Student> studentlist = new ArrayList<>();
+
+        for(int i = 0; i < gruppe.size(); i++){
+            if(gruppe.get(i) instanceof Prof){
+                proflist.add((Prof) gruppe.get(i));
+            } else if (gruppe.get(i) instanceof Student){
+                studentlist.add((Student) gruppe.get(i));
+            }
+        }
+
+        for(int i = 0; i < proflist.size(); i++){
+            System.out.println(proflist.get(i).vorstellen());
+        }
+
+        for(int i = 0; i < studentlist.size(); i++){
+            System.out.println(studentlist.get(i).vorstellen());
         }
     }
 }
 
-class Mensch {
+interface introducable {
+    String vorstellen();
+}
+
+abstract class Mensch {
     private int alter;
     private String name;
-    private String farbe;
+    private String geschlecht;
+
+    public String getGeschlecht() {
+        return geschlecht;
+    }
+
+    public void setGeschlecht(String geschlecht) {
+        this.geschlecht = geschlecht;
+    }
 
     public int getAlter() {
         return alter;
@@ -50,53 +83,95 @@ class Mensch {
         this.name = name;
     }
 
-    public String getFarbe() {
-        return farbe;
-    }
-
-    public void setFarbe(String farbe) {
-        this.farbe = farbe;
-    }
-
     public Mensch(){
         this(null, null, 0);
     }
 
-    public Mensch(String name, String farbe, int alter) {
+    public Mensch(String name, String geschlecht, int alter) {
         this.alter = alter;
         this.name = name;
-        this.farbe = farbe;
+        this.geschlecht = geschlecht;
     }
 
     @Override
     public String toString(){
-        return getName() + " / " + getAlter() + " / " + getFarbe();
+        return getName() + " / " + getAlter() + " / " + getGeschlecht();
     }
 }
 
-class Prof extends Mensch {
+class Prof extends Mensch implements introducable {
+    private static int nextNr = 1;
     private int nr;
-    
+
     public int getNr() {
         return nr;
     }
 
-    public void setNr(int nr) {
-        this.nr = nr;
-    }
-
-    public Prof(){
+    public Prof() {
         super();
-        this.nr = 0;
+        this.nr = nextNr++;
     }
 
-    public Prof(String name, String farbe, int alter, int nr){
-        super(name, farbe, alter);
-        this.nr = nr;
+    public Prof(String name, String geschlecht, int alter) {
+        super(name, geschlecht, alter);
+        this.nr = nextNr++;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " / Prof-Nr: " + nr;
+    }
+
+    @Override
+    public String vorstellen() {
+        return "Hallo, ich bin Professor, ich heiße " + getName() + ", ich bin " + getAlter() + " Jahre alt und bin " + getGeschlecht() + ". Meine Prof-Nummer ist " + nr + ".";
+    }
+}
+
+class Student extends Mensch implements introducable {
+    private static int nextMatnr = 1;
+    private int matnr;
+
+    public int getMatnr(){
+        return matnr;
+    }
+
+    public void setMatnr(int matnr){
+        this.matnr = nextMatnr++;
+    }
+
+    public Student() {
+        super();
+        this.matnr = nextMatnr++;
+    }
+
+    public Student(String name, int alter, String geschlecht){
+        super(name, geschlecht, alter);
+        this.matnr = nextMatnr++;
     }
 
     @Override
     public String toString(){
-        return super.toString() + " / " + getNr();
+        return super.toString() + " / Matrikelnummer: " + getMatnr();
+    }
+
+    @Override
+    public String vorstellen(){
+        return "Hallo, ich bin Student, ich heiße " + getName() + ", ich bin " + getAlter() + " Jahre alt und bin " + getGeschlecht() + ". Meine Matrikelnummer ist " + getMatnr() + ".";
+    }
+}
+
+class Mitarbeiter extends Mensch implements introducable {
+    public Mitarbeiter(){
+        super();
+    }
+
+    public Mitarbeiter(String name, int alter, String geschlecht){
+        super(name, geschlecht, alter);
+    }
+
+    @Override
+    public String vorstellen(){
+        return "Hallo, ich bin Mitarbeiter, ich heiße " + getName() + ", ich bin " + getAlter() + " Jahre alt und bin " + getGeschlecht();
     }
 }
